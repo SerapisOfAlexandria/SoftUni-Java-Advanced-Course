c2cb514bc26655d3c3cfafc5f10924f9c804f954
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-01-21 22:50:29 +0300

    "finished Read File problem"

---
+++ src/StreamsFilesAndDirectories/ReadFile.java
@@ -0,0 +1,21 @@
+package StreamsFilesAndDirectories;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\Serapis\\Desktop\\Java\\JavaAdvanced\\src\\StreamsFilesAndDirectories\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+
+        try (FileInputStream fileStream = new FileInputStream(path)) {
+            int oneByte = fileStream.read();
+            while (oneByte >= 0) {
+                System.out.print(Integer.toBinaryString(oneByte) + " ");
+                oneByte = fileStream.read();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
