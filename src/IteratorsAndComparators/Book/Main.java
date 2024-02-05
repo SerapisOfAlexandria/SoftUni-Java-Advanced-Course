e4533a4ad1cef648d4614d5ab72e1385506a9cf2
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-02-05 23:48:23 +0300

    "finished Book problem"

---
+++ src/IteratorsAndComparators/Book/Main.java
@@ -0,0 +1,9 @@
+package IteratorsAndComparators.Book;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
