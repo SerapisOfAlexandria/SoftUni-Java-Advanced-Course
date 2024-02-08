155ed5fb327c49a5ccffd90b1572ee31b21a78bc
Mona Lisa <slavi.nedqlkov@abv.bg>
2024-02-08 23:01:44 +0300

    "finished ListyIterator problem"

---
+++ src/IteratorsAndComparatorsExercise/ListyIterator/ListyIteratorClass.java
@@ -0,0 +1,35 @@
+package IteratorsAndComparatorsExercise.ListyIterator;
+
+public class ListyIteratorClass {
+
+    String[] strings;
+    int index = 0;
+
+    public ListyIteratorClass(String... strings) {
+        this.strings = strings;
+    }
+
+    public boolean hasNext() {
+        if (index == strings.length - 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean move() {
+        if (hasNext() && strings.length != 0) {
+            index++;
+            return true;
+        }
+        return false;
+    }
+
+    public void print() {
+        if (strings.length == 0) {
+            System.out.println("Invalid Operation!");
+        } else {
+            System.out.println(strings[index]);
+        }
+    }
+
+}
---
+++ src/IteratorsAndComparatorsExercise/ListyIterator/Main.java
@@ -0,0 +1,32 @@
+package IteratorsAndComparatorsExercise.ListyIterator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] strings = scanner.nextLine().replaceFirst("Create", "").trim().split(" ");
+        if (strings[0].equals("")) {
+            strings = new String[0];
+        }
+        ListyIteratorClass listyIterator = new ListyIteratorClass(strings);
+        String command = scanner.nextLine();
+
+        while (!command.equals("END")) {
+            switch (command) {
+                case "HasNext":
+                    System.out.println(listyIterator.hasNext());
+                    break;
+                case "Move":
+                    System.out.println(listyIterator.move());
+                    break;
+                case "Print":
+                    listyIterator.print();
+                    break;
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
